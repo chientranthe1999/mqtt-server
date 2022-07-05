0bc59854e84a9017c35f4ffcf2f17d21623c6c6e
chientt <chientt@kaopiz.com>
2022-07-06 00:17:13 +0700

    init serser

--- server/src/mqtt-client.js
+++ server/src/mqtt-client.js
@@ -1,5 +1,4 @@
 var mqtt = require('mqtt')
-const { saveGardenInfor } = require('./firebase.js')
 
 var options = {
   port: 1883,
@@ -16,27 +15,40 @@ const mqttClient = mqtt.connect('mqtt:broker.mqtt-dashboard.com', options)
 
 mqttClient.on('connect', function () {
   console.log('mqttClient connected')
-  mqttClient.subscribe('SmartGaden')
+  mqttClient.subscribe('WqiConnector')
 })
 
-const regex = /(?<date>.+)\/Node 1\/(?<air_humidity>.+)\/(?<soil_humidity>.+)\/(?<air_temperature>.+)\/(?<light>.+)/
-// air_humidity, soil_humidity, air_temperature, light
+const isBetween = (value, min, max) => {
+  return value >= min && value < max
+}
+
+const equal1 = (c, q, bp) => {
+  return ((q[1] - q[0]) * (bp[1] - c)) / (b[1] - b[0]) + q[1]
+}
+
+const initBOD = (bod) => {
+  let q = []
+  let bp = []
+  const val = [100, 75, 50, 25, 1]
+  const ranges = [
+    [4, 6],
+    [6, 15],
+    [15, 25],
+    [25, 50],
+    [50, 100]
+  ]
+
+  const findedIndex = ranges.findIndex((range) => isBetween(bod, range[0], range[1]))
+  bp[0] = val[findedIndex][0]
+  bp[1] = val[findedIndex][1]
+  q[0] = val[findedIndex]
+  q[1] = val[findedIndex + 1] ?? val[findedIndex]
+}
 
 mqttClient.on('message', function (topic, message) {
-  try {
-    const data = message.toString().match(regex)
-    const { date, air_humidity, soil_humidity, air_temperature, light } = data.groups
-    const id = new Date(date).getTime()
-    saveGardenInfor(id, {
-      date: id,
-      air_humidity,
-      soil_humidity,
-      air_temperature,
-      light
-    })
-  } catch (e) {
-    console.log(e)
-  }
+  const data = message.toString()
+
+  const [deviceId, turbidity, temperature] = data.split(' ')
 })
 
 module.exports = mqttClient
--- server/src/server.js
+++ server/src/server.js
@@ -10,22 +10,6 @@ app.use(cors())
 // Run MQTT client
 require('./mqtt-client.js')
 
-// init firebase
-const { getGardenInfor } = require('./firebase.js')
-
-// route
-app.get('/garden', async function (req, res) {
-  try {
-    const gardenInfor = await getGardenInfor()
-    if (Object.keys(gardenInfor).length) {
-      res.send(Object.values(gardenInfor))
-    }
-  } catch (e) {
-    console.log(e)
-    res.send([])
-  }
-})
-
 app.listen(port, () => {
   console.log('APP is running')
 })
